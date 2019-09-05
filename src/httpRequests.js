cf682b1e4759498b7f854036f7405490ce2d87b6
markuskonk <m_konk01@uni-muenster.de>
2019-09-05 14:23:26 +0200

    edits

--- src/httpRequests.js
+++ src/httpRequests.js
@@ -1,9 +1,5 @@
 const axios = require('axios');
 
-function searchBinding(term, metadata){
-    return axios.post(_env.api + 'bindings/searchBinding', {term:term, metadata: metadata});
-}
-
 function testRequest() {
     return axios.get("https://api.github.com/repos/MarkusKonk/Zenodo-test/contributors");
 }
