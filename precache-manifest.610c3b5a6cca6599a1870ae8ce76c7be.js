176764fc90b63853a1ad77404e64c117073d2256
markuskonk <m_konk01@uni-muenster.de>
2019-09-05 14:08:49 +0200

    Updates

---
+++ precache-manifest.610c3b5a6cca6599a1870ae8ce76c7be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535165aa81002528a873f433fc4f351c",
+    "url": "/softcite/index.html"
+  },
+  {
+    "revision": "abc153c5ea8190e4416a",
+    "url": "/softcite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c531c450be7ca774d67e",
+    "url": "/softcite/static/js/2.4db57918.chunk.js"
+  },
+  {
+    "revision": "abc153c5ea8190e4416a",
+    "url": "/softcite/static/js/main.cfc046c4.chunk.js"
+  },
+  {
+    "revision": "e5e49e6fc92e1ff5da90",
+    "url": "/softcite/static/js/runtime~main.5aa0728c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/softcite/static/media/logo.5d5d9eef.svg"
+  }
+]);
